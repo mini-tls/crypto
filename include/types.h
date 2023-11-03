383391cf3db286cd5069d2c6ae84d2129b20d36e
bmh-gh <27549756+bmh-gh@users.noreply.github.com>
2023-11-03 14:13:04 +0100

    add types to use in all crypto projects

---
+++ include/types.h
@@ -0,0 +1,15 @@
+//
+// Created by bmh on 03.11.23.
+//
+
+#ifndef CRYPTO_TYPES_H
+#define CRYPTO_TYPES_H
+
+#include <stdint.h>
+
+typedef uint8_t u8;
+typedef uint16_t u16;
+typedef uint32_t u32;
+typedef uint64_t u64;
+
+#endif //CRYPTO_TYPES_H
